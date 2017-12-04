92c9aeb54c632d1b73237206dc040544771d5651
Emilyyan <xw324@mail.missouri.edu>
2017-12-03 18:03:54 -0700

    first upload

---
+++ js/project2.js
@@ -0,0 +1,173 @@
+var table; 
+$(document).ready(function(){
+    table = $('#myTable').DataTable({
+            //"lengthMenu": [6],
+            "bLengthChange": false,
+            "iDisplayLength": 5,
+            "searching": false,
+            //"bPaginate": false,
+            "bFilter": true,
+            "bInfo": false,
+            "columnDefs": [
+                { "width": "10%", "targets": 3 }
+            ]
+            });
+    var ID;
+    var length = 0;
+
+    var db = firebase.database();
+    db.ref("/ZumbaMusic").on("value", function(snapshot){
+                
+                snapshot.forEach(function(childSnapshot) {
+                    //console.log(childSnapshot.val().ID);
+                    ID = childSnapshot.val().ID;
+                    var query = "select * from json where url='https://itunes.apple.com/us/lookup?id="+ID+"'";
+                    var params = {
+	                    q: query,
+	                    format: "json"
+	                }
+	                var url = 'http://query.yahooapis.com/v1/public/yql';
+	                $.getJSON(url, params, showResults);
+	                
+                });
+                
+                
+                var size = snapshot.numChildren();
+                //console.log(size);
+                
+    }); 
+    
+   
+   
+   //tour part
+   db.ref("/Tour").on("value", function(snapshot){
+                snapshot.forEach(function(childSnapshot) {
+                    console.log(childSnapshot.val());
+                    date = childSnapshot.val().time;
+                    artist =  childSnapshot.val().artist;
+                    place = childSnapshot.val().location;
+                    city = childSnapshot.val().city;
+                    tickets = childSnapshot.val().ticketsURL;
+                    
+                    var tourItem = `<div class="row">
+                                        <div class="col-md-2">${date}</div>
+                                        <div class='col-md-2'>${artist}</div>
+                                        <div class='col-md-2'>${place}</div>
+                                        <div class='col-md-2'>${city}</div>
+                                        <div class='col-md-2'><a target="_blank" href="${tickets}">tickets</a></div>`;
+                    $( "#tourList").append(tourItem);  
+                });
+   }); 
+   
+   //video part
+   db.ref("/ZumbaVideo").on("value", function(snapshot){
+                snapshot.forEach(function(childSnapshot) {
+                    videoID = childSnapshot.val().ID;
+                    videoName = childSnapshot.val().name;
+	                var url = "https://www.youtube.com/embed/"+videoID;
+	                //var iframeCode = "<iframe allowfullscreen frameborder='0' id='iframeYoutube' width='720' height='480' src='https://www.youtube.com/embed/" + videoID+ "></iframe>";
+	                var listCode = "<div class='row' style='margin-top:20px'><a style='font-weight: bold;font-size: 30px' class='video' id='"+ url+"'>"+videoName+"</a></div>";
+	                
+	                $( "#videoList").append(listCode);  
+	                //console.log($( "#videoList").html());
+	            
+                });
+                
+    }); 
+    
+    
+    $("#myModal").on("hidden.bs.modal",function(){
+        $("#iframeYoutube").attr("src","#");
+    });
+   
+    
+});
+
+function changeVideo(){
+  var url = $(this).attr("id");
+  var iframe=document.getElementById("iframeYoutube");
+  iframe.src=url;
+  //console.log($("#videoModal").html());
+  $("#myModal").modal("show");
+}
+
+$( "body" ).on( "click", ".video", changeVideo);
+
+
+var row_data = document.getElementById("data"); 
+var count = 1;
+
+function showResults(response){
+    var info = response.query.results.json.results;
+    console.log(info);
+    var thumbnail = info.artworkUrl100;
+    var song = info.trackCensoredName;
+    var artist = info.artistName;
+    var artistURL = info.artistViewUrl;
+    var previewURL = info.previewUrl;
+    var trackviewURL = info.trackViewUrl;
+    var album = info.collectionCensoredName;
+    var trackLinkCode = "<a target='_blank' href='" + trackviewURL + "'>" + album + "</span></a>";
+    var artistLinkCode =  "<a target='_blank' href='" + artistURL + "'>" + artist + "</span></a>";
+    //var URLicon = "<a target='_blank' href='" + trackviewURL + "'><span class='glyphicon glyphicon-music btn-lg'></span></a>"
+    var previewURLicon = "<a href='" + previewURL + "'><span class='glyphicon glyphicon-download btn-lg'></span></a>"
+
+    /*row_data.innerHTML += "<tr class='music-to-choose' data-thumb-nail='" + thumbnail + "'><th scope='row'>" + count + "</th><td class='info-name'>" + song + 
+                        "</td><td class='info-artist'>" + artist + "</td><td class='info-album'>" + album + "</td><td class='trackUrl'>"
+                        + URLicon + "</td><td class='previewUrl'>" + previewURLicon + "</td></tr>";*/
+    var rowNode = table.row.add([song, artistLinkCode, trackLinkCode, previewURLicon ]).draw().node();
+    $(rowNode).addClass("music-to-choose");
+    //$(rowNode).add("data-thumb-nail='" + thumbnail + "'");
+    $(rowNode).attr("data-thumb-nail", thumbnail);
+    $( rowNode ).find('td').eq(0).addClass('info-name');
+    $( rowNode ).find('td').eq(1).addClass('info-artist');
+    $( rowNode ).find('td').eq(2).addClass('info-album');
+    $( rowNode ).find('td').eq(3).addClass('previewUrl');
+
+    count++;
+
+    //var pic = response.query.results.json.results.artworkUrl100;
+    //console.log(pic);
+}
+
+//used to update info of player
+function getInfo() {
+    var thumbnail = $( this ).data("thumb-nail");
+  $( "#thumbnail").html ("<img class='thumbnail' src='"+thumbnail+"'>" );  
+  $( "#song" ).html("<p class='navbar-text'>" + $(this).children('.info-name').html() +"</p>");
+  $( "#artist" ).html("<p class='navbar-text'>" + $(this).children('.info-artist').children('a').text() +"</p>");
+  $( "#album" ).html("<p class='navbar-text'>" + $(this).children('.info-album').children('a').text() + "</p>");
+  var previewURL = $(this).children('.previewUrl').children('a').attr('href');
+  $( "#previewURL" ).html("<audio controls controlsList='nodownload' class='audio'><source src='" + previewURL + "' audio='m4a'></audio>" );
+  
+}
+$( "body" ).on( "click", ".music-to-choose", getInfo);//needs to use .on and bind it to body since the element is dynamically added
+
+
+//tour list section
+function getTours(){
+
+
+
+
+}
+
+
+
+//the scroll javascript code is from https://codepen.io/mattsince87/pen/exByn
+function scrollNav() {
+  $('.nav a').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top-60
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
---
+++ wave-player-test.js
@@ -0,0 +1,25 @@
+var ctx = document.createElement('canvas').getContext('2d');
+$('canvas').css('width', '800px');
+var linGrad = ctx.createLinearGradient(0, 64, 0, 200);
+linGrad.addColorStop(0.5, 'rgba(105,105,105, 1)');
+linGrad.addColorStop(0.5, 'rgba(105,105,105, 0.3)');
+
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: linGrad,
+    progressColor: 'hsla(200, 100%, 30%, 0.5)',
+    cursorColor: '#fff',
+    barWidth: 3,
+    height: 100
+});
+
+wavesurfer.on('loading', function (percents) {
+    document.getElementById('progress').value = percents;
+});
+
+wavesurfer.on('ready', function (percents) {
+    document.getElementById('progress').style.display = 'none';
+});
+
+wavesurfer.load('https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview30/v4/4f/42/0c/4f420c68-cbf1-47de-be68-927ccfdcef01/mzaf_5308035793306250766.plus.aac.p.m4a');
+
